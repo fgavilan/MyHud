83ef0ae6c22b67e0bb70282e7c5e07fb36a5c7e7
Usuario <Usuario>
2016-01-22 15:04:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/user/myhud/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.user.myhud;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private ListView list01;
+    private SensorManager sensorManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list01 = (ListView)findViewById(R.id.listView01);
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        List<String> listSensorType = new ArrayList<String>();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, listSensorType);
+        list01.setAdapter(adapter);
+        for (Sensor sensor : sensors) {
+            listSensorType.add(sensor.getName());
+        }
+    }
+}
